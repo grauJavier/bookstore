775d378d5f6d2b81f743302aad7640081dfdeb7e
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-14 00:56:57 -0400

    Add 'Header'

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/Header.css';
+import '../index.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div id="header__logo">
+        <h1 className='text-style-3'>Bookstore CMS</h1>
+      </div>
+      <nav id="navbar">
+        <ul id="navbar__list">
+          <li className="navbar__item--active text-style-6">Books</li>
+          <li className="navbar__item--inactive text-style-6">Categories</li>
+        </ul>
+        <div id="navbar__avatar">
+        </div>
+      </nav>
+    </header>
+  );
+}
